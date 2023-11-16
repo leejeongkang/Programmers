49aae8c8acc67771f762659e7121e6fcf5ed5fe9
강이정 <116529573+leejeongkang@users.noreply.github.com>
2023-11-16 15:30:32 +0900

    [level 2] Title: 프로세스, Time: 0.33 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/프로세스.js
+++ 프로그래머스/2/42587. 프로세스/프로세스.js
@@ -3,12 +3,15 @@ function solution(priorities, location) {
     const queue = priorities.map((val, index) => ({ val, index }));
 
     while (1) {
-        const queueValue = queue.shift();
-        if (queue.some(item => item.val > queueValue.val)) {
-            queue.push(queueValue);
+        const current = queue.shift();
+        if (queue.some(item => item.val > current.val)) {
+            queue.push(current);
+            console.log(current)
+             console.log(queue)
         } else {
             answer++;
-            if (queueValue.index === location) {
+            console.log(answer)
+            if (current.index === location) {
                 break;
             }
         }
